3f7da3661f9edd9b9a2b0065f2c088b60a2c3270
ziv talmon <zivtalmon89@gmail.com>
2022-10-19 14:23:41 +0300

    exercises 1,2 and 3 are complete

---
+++ Week2/Day2/ExerciseXP/script.js
@@ -0,0 +1,36 @@
+//Exercise1
+
+// let x = 10;
+// let y = 6; 
+
+// if (x > y) {
+//     alert("x is the biggest number");
+// } else {
+//     alert ("y is the biggest number")
+// }
+
+//Exercise2 
+
+// let newDog = "Chihuahua";
+
+// console.log(newDog.split("").length);
+// console.log(newDog.toUpperCase());
+// console.log(newDog.toLowerCase());
+
+// if (newDog == "Chihuahua") {
+//     alert("I love Chihuahuas, it\’\s my favorite dog breed");
+// } else {
+//     alert("I dont care, I prefer cats");
+// }
+
+//Exercise3
+
+const number = prompt("enter a number");
+
+if (number % 2 == 0) {
+    alert("x is an even number");
+} else {
+    alert("x is an odd number");
+}
+
+
