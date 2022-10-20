9428aef865e48821be061c6865aeb1fbd8bae474
ziv talmon <zivtalmon89@gmail.com>
2022-10-21 00:22:56 +0300

    Exercise2 complete, with the side effect of outputting all 3 loops and logging quite a long message, haven't figured out how to control that exactly

--- Week2/Day3/ExerciseXP/script.js
+++ Week2/Day3/ExerciseXP/script.js
@@ -16,36 +16,66 @@
 // Write code that gives the index of “Foo”. Why does it return -1 ?
 
 // Create a variable called last which value is the last element of the array.
-// Hint: What is the relationship between the index of the last element in the array and the length of the array?
+// // Hint: What is the relationship between the index of the last element in the array and the length of the array?
 
-const people = ["Greg", "Mary", "Devon", "James"];
+// const people = ["Greg", "Mary", "Devon", "James"];
 
-people.shift();
-console.log(people);
+// people.shift();
+// console.log(people);
 
-people.splice(2, 1, "Jason");
-console.log(people);
+// people.splice(2, 1, "Jason");
+// console.log(people);
 
-people.push("Ziv");
-console.log(people);
+// people.push("Ziv");
+// console.log(people);
 
-let maryIndex= people.indexOf("Mary");
-console.log(maryIndex);
+// let maryIndex= people.indexOf("Mary");
+// console.log(maryIndex);
 
-let peopleCopy = people.slice(1,3);
-console.log(peopleCopy);
+// let peopleCopy = people.slice(1,3);
+// console.log(peopleCopy);
 
-let fooIndex = people.indexOf("Foo");
-console.log(fooIndex);
+// let fooIndex = people.indexOf("Foo");
+// console.log(fooIndex);
 
-let last = people.length;
-console.log(last - 1);
+// let last = people.length;
+// console.log(last - 1);
 
-for (let i = 0; i < people.length; i++) {
-    console.log(people[i]);
+// for (let i = 0; i < people.length; i++) {
+//     console.log(people[i]);
 
-    if (people[i] == "Jason") {
-        break;
+//     if (people[i] == "Jason") {
+//         break;
+//     }
+// };
+
+//Exercise 2 
+
+// Create an array called colors where the value is a list of your five favorite colors.
+// Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
+// Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
+// Hint : create an array of suffixes to do the Bonus
+
+let colors = ["red", "blue", "black", "green", "purple"];
+let numbers = [1,2,3,4,5,6];
+let suffixes = ["st", "nd", "rd", "th", "th"];
+{
+for (let i = 0; i < Number.length; i++)
+for (let i = 0; i < colors.length; i++)
+for (let i = 0; i < suffixes.length; i++) {
+
+ 
+    if (numbers[i] == 1 && suffixes[i] == suffixes[0]) {
+    console.log(`My ${numbers[i]}${suffixes[i]} choice is ${colors[i]}`);
+    } else if (numbers[i] == 2 && suffixes[i] == suffixes[1]) {
+        console.log(`My ${numbers[i]}${suffixes[i]} choice is ${colors[i]}`);
+    } else if (numbers[i] == 3 && suffixes[i] == suffixes[2]) {
+        console.log(`My ${numbers[i]}${suffixes[i]} choice is ${colors[i]}`);
+    } else if (numbers[i] == 4 && suffixes[i] == suffixes[3]) {
+        console.log(`My ${numbers[i]}${suffixes[i]} choice is ${colors[i]}`);
+    } else {
+        console.log(`My ${numbers[i]}${suffixes[i]} choice is ${colors[i]}`);
     }
-}
 
+}
+}
