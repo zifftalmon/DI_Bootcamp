5f8cc6946c584c5b73ebb950a9d272a83fef342c
ziv talmon <zivtalmon89@gmail.com>
2022-10-21 00:48:14 +0300

    daily challange complete

---
+++ Week2/Day3/Daily-Challenge/script.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < 1; i++) {
+    console.log(i);
+  for(let j = 0; j < 1; j++) {
+    console.log(i,j);
+    for(let k = 0; k < 1; k++) {
+        console.log(i,j,k);
+        for(let l = 0; l < 1; l++) {
+            console.log(i,j,k,l);
+            for(let m = 0; m < 1; m++) {
+                console.log(i,j,k,l,m);
+                for(let n = 0; n < 1; n++) {
+                    console.log(i,j,k,l,m,n);
+                  }
+              }
+          }
+      }
+  }
+
+}
