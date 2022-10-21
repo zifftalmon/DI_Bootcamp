8a10d3d5387f001a54ad63eb5dbaa3cc701bb11e
ziv talmon <zivtalmon89@gmail.com>
2022-10-22 01:47:41 +0300

    Exercise 2 complete

--- Week2/Day3/ExerciseXPGold/script.js
+++ Week2/Day3/ExerciseXPGold/script.js
@@ -1,10 +1,35 @@
-let numbers = [123, 8409, 100053, 333333333, 7];
+//Exercise 1
 
-for (let i of numbers) {
-    if (i%3 == 0) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-}
+// let numbers = [123, 8409, 100053, 333333333, 7];
 
+// for (let i of numbers) {
+//     if (i%3 == 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+
+//Exercise 2
+
+
+
+let guestList = {
+    randy: "Germany",
+    karla: "France",
+    wendy: "Japan",
+    norman: "England",
+    sam: "Argentina"
+  }
+  let student = prompt("what is your name?");
+
+  if (student in guestList === true) {
+    console.log(`Hi! I'm, ${student} and I'm from ${guestList[student]}.`);
+  } else {
+    console.log (`Hi! I'm a guest.`);
+  } 
+
+  /*
+  i reached the solution accidentaly
+  */
