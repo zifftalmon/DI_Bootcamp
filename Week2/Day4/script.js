83eb5aaa422b664bb8d4d982b32b6055679760ed
ziv talmon <zivtalmon89@gmail.com>
2022-10-22 20:07:05 +0300

    Exercise 4 at work, order in which to write syntax is challenging

--- Week2/Day4/script.js
+++ Week2/Day4/script.js
@@ -69,17 +69,66 @@
 
 // At the end, console.log the sum of all numbers that are divisible by 23.
 
+// function isDivisible(){
+//     let sum = 0;
+//     for (let i = 0; i < 500; i++) {
+//         if(i % 23 == 0) {
+//             sum += i;
+//             console.log(i, sum);  
+//         }
+//     }
+// }
+// isDivisible();
+
+// Bonus: Add a parameter divisor to the function.
+
+// function isDivisible(divisor){
+//     let sum = 0;
+//     for (let i = 0; i < 500; i++) {
+//         if(i % divisor == 0) {
+//             sum += i;
+//             console.log(i, sum);  
+//         }
+//     }
+// }
+// isDivisible(5);
+
+
 //Exercise4
 
-function isDivisible(){
-    let sum = 0;
-    for (let i = 0; i < 500; i++) {
-        if(i % 23 == 0) {
-            sum += i;
-            console.log(i, sum);
-            
-        }
-    }
-}
-isDivisible();
+// Add the stock and prices objects to your js file.
+
+// Create an array called shoppingList with the following items:
+//  “banana”, “orange”, and “apple”.
+//  It means that you have 1 banana, 1 orange and 1 apple in your cart.
+
+// Create a function called myBill() that takes no parameters.
+
+// The function should return the total price of your shoppingList. In order to do this you must follow these rules:
+// The item must be in stock. (Hint : check out if .. in)
+// If the item is in stock find out the price in the prices object.
+
+// Call the myBill() function.
+
+// Bonus: If the item is in stock, decrease the item’s stock by 1
+
+
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
+
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+} 
+
+const shoppingList = ["banana", "orange", "apple"];
+
 
