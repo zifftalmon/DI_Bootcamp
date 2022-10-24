dc4cfbd254491e04ba29ef04ff41113033b278f6
ziv talmon <zivtalmon89@gmail.com>
2022-10-25 01:23:26 +0300

    couldn't exactly attach one part to the other

--- Week2/Day5/Mini-project-Play-the-game/script.js
+++ Week2/Day5/Mini-project-Play-the-game/script.js
@@ -1,38 +1,56 @@
 function playTheGame () {
     let game = confirm("Would you like to play the game?");
     if (game == false) {
-        return alert("No problem, Goodbye");
-    } else  {
-        return prompt("please neter a number between 1 - 10")
+         alert("No problem, Goodbye");
+         return
     }
+    
+    
+let computerNumber = hiddenNumber(); 
+let userNumber = prompt("please Enter a Number");
+while (userNumber != computerNumber) {
+
+if(numberValidity(userNumber)) {
+    alert("Sorry not a number");
+    return
 }
 
+if(range(userNumber)) {
+    alert("sorry it\'s not a good number, Goodbye");
+    return
+}
+userNumber = prompt("please Enter a Number");
 
-let userNumber = playTheGame();
-
-function numberValidity () {
-    if (userNumber.match("[a-z]+")) {
-        return alert("Sorry Not a number, Goodbye");
-    } else if (userNumber > 10 || userNumber < 0) {
-        return alert("sorry it\'s not a good number, Goodbye")
-    } else {
-        return userNumber;
-    }
+}
+if (computerNumber > userNumber) {
+alert("Your number is bigger then the computer's, guess again");
+}
+if (computerNumber < userNumber) {
+alert("Your number is smaller than the computer's, guess again");
 }
 
-let computerNumber = Math.floor(Math.random() * 10);
+}
+
+function numberValidity (str) {
+    return str.match("[a-z]+")
+ }
+ 
+ function range (number) {
+     return number >= 10 || number <= 0;
+ }
+ 
+ function hiddenNumber () {
+     Math.floor(Math.random() * 10);
+ }
+
 
 function compareNumbers (userNumber,computerNumber) {
-    if (userNumber == computerNumber) {
-        return alert("WINNER"); 
-    } else if (computerNumber < userNumber) {
-        return prompt("Your number is smaller than the computer's, guess again");
-    } else if (computerNumber > userNumber) {
-        return prompt("Your number is bigger then the computer's, guess again");
-    }  else {
-        return alert("out of chances");
-    }
+if(userNumber == computerNumber) {
+ alert("Winner"); 
+ return 
+}
 }
 
 
-compareNumbers(userNumber, computerNumber);
+
+// compareNumbers(userNumber, computerNumber)
