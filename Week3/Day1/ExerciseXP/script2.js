50eb46c782a076ad3635ac3766dcbd76c69573be
ziv talmon <zivtalmon89@gmail.com>
2022-10-25 19:50:37 +0300

    second exercise complete

---
+++ Week3/Day1/ExerciseXP/script2.js
@@ -0,0 +1,13 @@
+let divColor = document.querySelector("div");
+
+divColor.style.backgroundColor = "lightblue";
+divColor.style.padding = "2em";
+
+let li = document.getElementsByTagName("li");
+
+li[0].style.visibility = "hidden";
+li[1].style.border = "3px solid black";
+
+let body = document.getElementsByTagName("body")[0];
+
+body.style.fontSize = "30px";
