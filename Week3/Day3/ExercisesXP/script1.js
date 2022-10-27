74305c418d4ab87f3f7a15ca0fbe48a5e23de6eb
ziv talmon <zivtalmon89@gmail.com>
2022-10-27 13:00:30 +0300

    exercise 1 nearly complete

--- Week3/Day3/ExercisesXP/script1.js
+++ Week3/Day3/ExercisesXP/script1.js
@@ -1,23 +1,45 @@
-function helloWorld () {
-    alert("hello World");
-} 
+// function helloWorld () {
+//     alert("hello World");
+// } 
 
 
-let div = document.getElementById("container");
+// let div = document.getElementById("container");
+
+// function newPara() {
+//     let newP = document.createElement("p");
+//     let hello = document.createTextNode("Hello World");
+//     newP.appendChild(hello);
+//     div.appendChild(newP);
+// }
+
+// setTimeout(helloWorld, 2000);
+// setTimeout(newPara, 2000);
+
+
+
 
 function newPara() {
+    let div = document.getElementById("container");
     let newP = document.createElement("p");
     let hello = document.createTextNode("Hello World");
     newP.appendChild(hello);
-    div.appendChild(newP);
+    div.appendChild(newP)
 }
 
-setTimeout(helloWorld, 2000);
-setTimeout(newPara, 2000);
 let button = document.getElementById("clear");
 
-function clearTimeout () {
-clearTimeout(helloWorld);
+function clearButton () {
+clearInterval(timer);
+console.log("stop");
 }
 
-button.addEventListener("click", clearTimeout)
+button.addEventListener("click", clearButton)
+let timer = setInterval(newPara, 2000);
+
+let pCount = document.querySelectorAll("p");
+
+function pInterval () {
+    if (pCount == 5) {
+        clearInterval(timer);
+    }
+}
