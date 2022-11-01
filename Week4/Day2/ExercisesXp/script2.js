a5689440b107f6640bbae080dd52059dd524ade1
ziv talmon <zivtalmon89@gmail.com>
2022-11-02 00:30:57 +0200

    exercise complete

---
+++ Week4/Day2/ExercisesXp/script2.js
@@ -0,0 +1,16 @@
+// function kgToGrams (number) {
+//     number *= 1000;
+//     console.log(`${number}gr`)
+// }
+// kgToGrams(3);
+
+// let kgToGrams = function (number) {
+//     number *= 1000;
+//     console.log(`${number}gr`)
+// }
+// kgToGrams(2);
+
+//DIFFERENNCE BETWEEN A FUNCTION DECLARATION AND FUNCTION EXPRESIION IS THE ASSIGNMENT TO A VARIABLE AN THE USAGE OF AN ANONYMMOUS FUNCTION
+
+// let kgToGrams = (number) => {number *= 1000; console.log(`${number}gr`)}
+// kgToGrams(1);
