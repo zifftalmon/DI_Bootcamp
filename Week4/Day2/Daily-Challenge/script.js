9d26b7679cd866248a7236a2ddf7325a791747eb
ziv talmon <zivtalmon89@gmail.com>
2022-11-02 00:12:57 +0200

    daily challenge complete

---
+++ Week4/Day2/Daily-Challenge/script.js
@@ -0,0 +1,29 @@
+let client = "John";
+
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        payed : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+let displayGroceries = () => {
+    groceries.fruits.forEach(element => {
+        console.log(element);
+    })
+}
+displayGroceries();
+
+let cloneGroceries = () => {
+    let user = client;
+    client = "Betty"
+    console.log(user, client);
+    let shopping = groceries;
+    shopping.totalPrice = "35$";
+    shopping.other.payed = false;
+    console.log(shopping);
+    console.log(groceries);
+}
