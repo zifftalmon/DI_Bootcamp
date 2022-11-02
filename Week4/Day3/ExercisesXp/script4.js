536434f8159365b10fc502366b1ff92b80660572
ziv talmon <zivtalmon89@gmail.com>
2022-11-02 19:19:17 +0200

    exercise complete

---
+++ Week4/Day3/ExercisesXp/script4.js
@@ -0,0 +1,14 @@
+const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
+               {name: "Liam", course: "Computer Science", isPassed: false}, 
+               {name: "Jenner", course: "Information Technology", isPassed: true}, 
+               {name: "Marco", course: "Robotics", isPassed: true}, 
+               {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+               {name: "Jamie", course: "Big Data", isPassed: false}];
+
+
+
+let passed = students.filter(element => element.isPassed == true);
+
+passed.forEach(element => {
+    console.log(`good job ${element.name} you passed the course in ${element.course}`);
+})
