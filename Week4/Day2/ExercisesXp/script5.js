b39f2e323cdfbe8e9891d2cdfc397fd10a719ef4
ziv talmon <zivtalmon89@gmail.com>
2022-11-02 18:21:02 +0200

    exercise complete

--- Week4/Day2/ExercisesXp/script5.js
+++ Week4/Day2/ExercisesXp/script5.js
@@ -1,9 +1,23 @@
 function makeJuice (size) {
+    let ingredients = [];
     addIngredients("kiwi", "apple", "carrot");
-        function addIngredients (one, two, three) {
-            let para = document.getElementById("para");
-            let text = document.createTextNode(`The client wants a ${size} juice, containing ${one}, ${two}, ${three}`);
-            para.appendChild(text);
-    }
+    console.log(size);
+    displayJuice(ingredients);  
+
+function addIngredients (one, two, three) {
+    ingredients.push(one, two, three);   
 }
-makeJuice("small");
+
+function displayJuice(ingredients) {
+    let part1 = `The client wants a ${size} juice, containing `;         
+    let part2 = "";
+    ingredients.forEach((element => {
+        part2 += element + ', '
+    }));
+    let sentence = part1 + part2; 
+    console.log(sentence);
+}
+}
+makeJuice("large");
+
+
