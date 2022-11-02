23f1ff14e039c7b5f4bff895f4b373ccfb7f1cb3
ziv talmon <zivtalmon89@gmail.com>
2022-11-02 23:08:18 +0200

    bmi challenge complete

--- Week2/Day3/ExerciseXPNinja/script.js
+++ Week2/Day3/ExerciseXPNinja/script.js
@@ -12,33 +12,65 @@
 // Display the name of the person who has the largest BMI.
 
 
+function bmi(a,b) {
+    return a / Math.pow(b,2);
+}
+
 const person1 = {
     FullName: "david hasselhof",
     Mass: 90,
     Height: 1.80,
-    Bmi: function() {
-        console.log(this.Mass / Math.pow(this.Height,2));
-    },
+    Bmi: bmi (90, 1.80),
 }
 
 const person2 = {
     FullName: "gary hasselhof",
     Mass: 110,
     Height: 1.40,
-    Bmi: function() {
-        console.log(this.Mass / Math.pow(this.Height,2));
-    },
+    Bmi: bmi(110,1.40),
 }
-
-person1.Bmi();
-person2.Bmi();
 
 function comparison () {
-   let bmiNum = Math.max(person1["Bmi"], person2["Bmi"]);
-   return bmiNum;
-   
+    let bmiNum = Math.max(bmi1,bmi2)
+    return bmiNum;
 }
+
+let bmi1 = person1.Bmi;
+
+let bmi2 = person2.Bmi;
 
 let bigger = comparison();
 
 console.log(bigger);
+
+
+// const person1 = {
+//     FullName: "david hasselhof",
+//     Mass: 90,
+//     Height: 1.80,
+//     Bmi: function() {
+//         console.log(this.Mass / Math.pow(this.Height,2));
+//     },
+// }
+
+// const person2 = {
+//     FullName: "gary hasselhof",
+//     Mass: 110,
+//     Height: 1.40,
+//     Bmi: function() {
+//         console.log(this.Mass / Math.pow(this.Height,2));
+//     },
+// }
+
+// person1.Bmi();
+// person2.Bmi();
+
+// function comparison () {
+//    let bmiNum = Math.max(person1["Bmi"], person2["Bmi"]);
+//    return bmiNum;
+   
+// }
+
+// let bigger = comparison();
+
+// console.log(bigger);
