607f94e1c763d34f6b73e716504457d958328391
ziv talmon <zivtalmon89@gmail.com>
2022-11-02 11:54:47 +0200

    indentation added

--- Week4/Day2/ExercisesXp/script5.js
+++ Week4/Day2/ExercisesXp/script5.js
@@ -1,9 +1,9 @@
 function makeJuice (size) {
     addIngredients("kiwi", "apple", "carrot");
-    function addIngredients (one, two, three) {
-        let para = document.getElementById("para");
-        let text = document.createTextNode(`The client wants a ${size} juice, containing ${one}, ${two}, ${three}`);
-        para.appendChild(text);
+        function addIngredients (one, two, three) {
+            let para = document.getElementById("para");
+            let text = document.createTextNode(`The client wants a ${size} juice, containing ${one}, ${two}, ${three}`);
+            para.appendChild(text);
     }
 }
 makeJuice("small");
