8e72f8a42d38dedb2fe7dc43ac0cfdb82fba2e3c
ziv talmon <zivtalmon89@gmail.com>
2022-11-02 18:30:31 +0200

    exercise complete

---
+++ Week4/Day3/ExercisesXp/script1.js
@@ -0,0 +1,22 @@
+// // ------1------
+// const fruits = ["apple", "orange"];
+// const vegetables = ["carrot", "potato"];
+
+// const result = ['bread', ...vegetables, 'chicken', ...fruits];
+
+// // OUTPUT WILL BE [BREAD,CARROT,POTATO,CHICKEN,APPLE,ORANGE];
+
+// console.log(result);
+
+// ------2------
+// const country = "USA";
+// console.log([...country]);
+
+// //OUTPUT WILL BE [U,S,A]
+
+// ------Bonus------
+let newArray = [...[,,]];
+console.log(newArray);
+
+//OUTPUT IS [UNDEFINED,UNDEFINED] BECAUSE THE OPERATOR OPERATES ON THE COMMAS
+
