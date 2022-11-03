1b5d61dae6010fed3394b315811dd3b68c926d22
ziv talmon <zivtalmon89@gmail.com>
2022-11-03 22:02:01 +0200

    exercise3 complete

--- Week2/Day4/ExercisesXpGold/script.js
+++ Week2/Day4/ExercisesXpGold/script.js
@@ -1,21 +1,36 @@
-//exercise 1
-let isBlank = (string) => {
-    if (string === " ") {
-        return true
-    } else {
-        return false
-    }
-}
+// //exercise 1
+// let isBlank = (string) => {
+//     if (string === " ") {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
 
-console.log(isBlank(" "));
+// console.log(isBlank(" "));
 
-//exercise 2
+// //exercise 2
 
-let abbrevName = (firstName, lastName) => {
-    if (lastName) {
-        return `${firstName} ${lastName[0].toUpperCase()}.`;
-    }
-}
+// let abbrevName = (firstName, lastName) => {
+//     if (lastName) {
+//         return `${firstName} ${lastName[0].toUpperCase()}.`;
+//     }
+// }
 
-console.log(abbrevName("henry", "williams"))
+// console.log(abbrevName("henry", "williams"))
 
+//exercise 3
+// let sentence = " ";
+
+// let switchCase = (input) => {
+//     for (let i = 0; i < input.length; i++) {
+//         if (input[i] === input[i].toLowerCase()) {
+//             sentence += input[i].toUpperCase();
+//         } else {
+//             sentence += input[i].toLowerCase();
+//         }
+//     }
+//     console.log(sentence);
+//     return sentence;
+// }
+// switchCase("How aRE We ToDAy");
