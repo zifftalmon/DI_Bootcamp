31f09b420f15a136a74b2494aa9ef7f1b0c701ae
ziv talmon <zivtalmon89@gmail.com>
2022-11-04 18:44:01 +0200

    function for robot divs added, DOM for appending robot deatails to divs added

--- Week4/Day5/Mini-Project-Robots/script.js
+++ Week4/Day5/Mini-Project-Robots/script.js
@@ -1,12 +1,55 @@
+let robots = document.getElementById("robots");
+
+let robotBox;
+
+let robotDiv = () => {
+    for(let i=0; i < 10; i++) {
+        robotBox = document.createElement("div");
+        robots.appendChild(robotBox);
+        robotBox.style.background = "#1bd7c1";
+        robotBox.setAttribute("class", "itemPara");
+    }
+}
+robotDiv();
 
 
+class Robot {
+    constructor (id,name,username,email,image) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.image = image;
+    }
+}
+
+const robot1 = new Robot ("1", "Leanne Graham","Bret","Sincere@april.biz","https://robohash.org/1?200x200");
+
+console.log(robot1);
+
+let itemPara = document.querySelector(".itemPara");
+
+let header1 = document.createElement("h2");
+
+let headerText = document.createTextNode(`${robot1.name}`);
+
+let email1 = document.createElement("p");
+
+let emailText = document.createTextNode(`${robot1.email}`);
+
+let image1 = document.createElement("img");
+
+image1.setAttribute("src",`${robot1.image}`)
 
 
+header1.append(headerText);
+email1.append(emailText);
 
+itemPara.append(image1);
 
+itemPara.append(header1);
 
-
-
+itemPara.append(email1);
 
 
 // const robots = [
