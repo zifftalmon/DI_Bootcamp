6619c7522c320a57b142b938c14c4dedd8f42e9b
ziv talmon <zivtalmon89@gmail.com>
2022-11-04 21:38:06 +0200

    no changes on html file, styles changed on item para in css file, function for robot divs is WIP

--- Week4/Day5/Mini-Project-Robots/script.js
+++ Week4/Day5/Mini-Project-Robots/script.js
@@ -7,6 +7,7 @@ let robotDiv = () => {
         robotBox = document.createElement("div");
         robots.appendChild(robotBox);
         robotBox.style.background = "#1bd7c1";
+        robotBox.setAttribute("id","robotBoxDiv")
         robotBox.setAttribute("class", "itemPara");
     }
 }
@@ -23,104 +24,112 @@ class Robot {
     }
 }
 
-const robot1 = new Robot ("1", "Leanne Graham","Bret","Sincere@april.biz","https://robohash.org/1?200x200");
+// const robot1 = new Robot ("1", "Leanne Graham","Bret","Sincere@april.biz","https://robohash.org/1?200x200");
 
-console.log(robot1);
-
-let itemPara = document.querySelector(".itemPara");
-
-let header1 = document.createElement("h2");
-
-let headerText = document.createTextNode(`${robot1.name}`);
-
-let email1 = document.createElement("p");
-
-let emailText = document.createTextNode(`${robot1.email}`);
-
-let image1 = document.createElement("img");
-
-image1.setAttribute("src",`${robot1.image}`)
+// console.log(robot1);
 
 
-header1.append(headerText);
-email1.append(emailText);
 
-itemPara.append(image1);
+const robotsArray = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'https://robohash.org/1?200x200'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        image: 'https://robohash.org/2?200x200'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'https://robohash.org/3?200x200'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'https://robohash.org/4?200x200'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'https://robohash.org/5?200x200'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'https://robohash.org/6?200x200'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'https://robohash.org/7?200x200'
+    },
+    {
+        id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'https://robohash.org/8?200x200'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'https://robohash.org/9?200x200'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+    ];
 
-itemPara.append(header1);
+const result = [];
 
-itemPara.append(email1);
-
-
-// const robots = [
-//     {
-//       id: 1,
-//       name: 'Leanne Graham',
-//       username: 'Bret',
-//       email: 'Sincere@april.biz',
-//       image: 'https://robohash.org/1?200x200'
-//     },
-//     {
-//       id: 2,
-//       name: 'Ervin Howell',
-//       username: 'Antonette',
-//       email: 'Shanna@melissa.tv',
-//       image: 'https://robohash.org/2?200x200'
-//     },
-//     {
-//       id: 3,
-//       name: 'Clementine Bauch',
-//       username: 'Samantha',
-//       email: 'Nathan@yesenia.net',
-//       image: 'https://robohash.org/3?200x200'
-//     },
-//     {
-//       id: 4,
-//       name: 'Patricia Lebsack',
-//       username: 'Karianne',
-//       email: 'Julianne.OConner@kory.org',
-//       image: 'https://robohash.org/4?200x200'
-//     },
-//     {
-//       id: 5,
-//       name: 'Chelsey Dietrich',
-//       username: 'Kamren',
-//       email: 'Lucio_Hettinger@annie.ca',
-//       image: 'https://robohash.org/5?200x200'
-//     },
-//     {
-//       id: 6,
-//       name: 'Mrs. Dennis Schulist',
-//       username: 'Leopoldo_Corkery',
-//       email: 'Karley_Dach@jasper.info',
-//       image: 'https://robohash.org/6?200x200'
-//     },
-//     {
-//       id: 7,
-//       name: 'Kurtis Weissnat',
-//       username: 'Elwyn.Skiles',
-//       email: 'Telly.Hoeger@billy.biz',
-//       image: 'https://robohash.org/7?200x200'
-//     },
-//     {
-//       id: 8,
-//       name: 'Nicholas Runolfsdottir V',
-//       username: 'Maxime_Nienow',
-//       email: 'Sherwood@rosamond.me',
-//       image: 'https://robohash.org/8?200x200'
-//     },
-//     {
-//       id: 9,
-//       name: 'Glenna Reichert',
-//       username: 'Delphine',
-//       email: 'Chaim_McDermott@dana.io',
-//       image:'https://robohash.org/9?200x200'
-//     },
-//     {
-//       id: 10,
-//       name: 'Clementina DuBuque',
-//       username: 'Moriah.Stanton',
-//       email: 'Rey.Padberg@karina.biz',
-//       image:'https://robohash.org/10?200x200'
-//     }
-//     ];
+for (const robot of robotsArray) {
+    const id = robot.id;
+    const name = robot.name;
+    const username = robot.username;
+    const email = robot.email;
+    const image = robot.image;
+    const newRobot = new Robot (id,name,username,email,image);
+    result.push(newRobot);
+}
+    console.log(result);
+    function addRobots() {
+        robotsArray.forEach ((result) => {
+            let itemPara = document.querySelector("#robotBoxDiv");
+            let header1 = document.createElement("h2");
+            let headerText = document.createTextNode(`${result.name}`);
+            let email1 = document.createElement("p");
+            let emailText = document.createTextNode(`${result.email}`);
+            let image1 = document.createElement("img");
+            image1.setAttribute("src",`${result.image}`);
+            header1.append(headerText);
+            email1.append(emailText);
+            itemPara.append(image1);
+            itemPara.append(header1);
+            itemPara.append(email1);
+        })  
+    }
+addRobots();
+    // console.log(i); 
