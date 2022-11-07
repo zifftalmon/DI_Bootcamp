adda1ff256ddd40011e5d1209d21a2d393c72384
ziv talmon <zivtalmon89@gmail.com>
2022-11-07 16:59:41 +0200

    xhr operations enclosed in functions

--- Week5/Day2/ExercisesXp/script.js
+++ Week5/Day2/ExercisesXp/script.js
@@ -1,4 +1,4 @@
-// const xhr = new XMLHttpRequest();
+// const xhr1 = new XMLHttpRequest();
 
 // xhr.open("GET", "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
 
@@ -8,12 +8,32 @@
 
 // console.log(xhr);
 
-const xhr = new XMLHttpRequest();
+// function getGiphy () {
+        
+//     xhr1.open("GET", "https://api.giphy.com/v1/gifs/search?q=sun&limit=10&offset=2&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
 
-xhr.open("GET", "https://api.giphy.com/v1/gifs/search?q=sun&limit=10&offset=2&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
+//     xhr1.responseType = "json";
 
-xhr.responseType = "json";
+//     xhr1.send();
 
-xhr.send();
+//     console.log(xhr1);
 
-console.log(xhr);
+// }
+
+// getGiphy();
+
+// const xhr2 = new XMLHttpRequest();
+
+// function getSun () {
+        
+//     xhr2.open("GET", "https://api.giphy.com/v1/gifs/search?q=sun&limit=10&offset=2&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
+
+//     xhr2.responseType = "json";
+
+//     xhr2.send();
+
+//     console.log(xhr2);
+
+// }
+
+// getSun();
