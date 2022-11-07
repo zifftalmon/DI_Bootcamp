6229bd88018e02bb622f786fa6e967794be02a0d
ziv talmon <zivtalmon89@gmail.com>
2022-11-07 17:28:42 +0200

    js src changed

--- Week5/Day2/ExercisesXp/script.js
+++ Week5/Day2/ExercisesXp/script.js
@@ -1,28 +1,20 @@
-// const xhr1 = new XMLHttpRequest();
+const xhr1 = new XMLHttpRequest();
 
-// xhr.open("GET", "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
-
-// xhr.responseType = "json";
-
-// xhr.send();
-
-// console.log(xhr);
-
-// function getGiphy () {
+function getGiphy () {
         
-//     xhr1.open("GET", "https://api.giphy.com/v1/gifs/search?q=sun&limit=10&offset=2&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
+    xhr1.open("GET", "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
 
-//     xhr1.responseType = "json";
+    xhr1.responseType = "json";
 
-//     xhr1.send();
+    xhr1.send();
 
-//     console.log(xhr1);
+    console.log(xhr1);
 
-// }
+}
 
-// getGiphy();
+getGiphy();
 
-// const xhr2 = new XMLHttpRequest();
+const xhr2 = new XMLHttpRequest();
 
 // function getSun () {
         
