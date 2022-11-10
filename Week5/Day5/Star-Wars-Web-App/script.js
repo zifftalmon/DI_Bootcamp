12860d66725a2e31e48d75c87e6eb27c21977115
ziv talmon <zivtalmon89@gmail.com>
2022-11-10 23:09:59 +0200

    function for appending data to page complete, loading animation and work on error handling remains

--- Week5/Day5/Star-Wars-Web-App/script.js
+++ Week5/Day5/Star-Wars-Web-App/script.js
@@ -2,47 +2,35 @@ const button = document.getElementById("search");
 
 button.addEventListener("click", findPerson);
 
-
-
 async function findPerson (e)  {
     e.preventDefault();
     const number = Math.floor(Math.random() * 60) + 1;
-    console.log(number);
 
     const name = await fetch(`https://www.swapi.tech/api/people/${number}`)
     const response1 = await name.json();
     const result1 = await response1.result.properties.name;
-    // console.log(result1);
-
     
     const height = await fetch(`https://www.swapi.tech/api/people/${number}`)
     const response2 = await height.json();
     const result2 = await response2.result.properties.height;
-    // console.log(result2);
-
     
     const gender = await fetch(`https://www.swapi.tech/api/people/${number}`)
     const response3 = await gender.json();
     const result3 = await response3.result.properties.gender;
-    // console.log(result3);
 
-    
     const age = await fetch(`https://www.swapi.tech/api/people/${number}`)
     const response4 = await age.json();
     const result4 = await response4.result.properties.birth_year;
-    // console.log(result4);
 
     const planet = await fetch(`https://www.swapi.tech/api/planets/${number}`)
     const response5 = await planet.json();
     const result5 = await response5.result.properties.name;
-    // console.log(result5);
 
     async function data () {
         try {
             const allPromises = Promise.all([result1, result2, result3, result4, result5])
             const promises = await allPromises;
             const result = await promises;
-            console.log(result);
             appendToPage(result);
         }catch {
 
@@ -53,14 +41,30 @@ async function findPerson (e)  {
     function appendToPage (arr) {
         const info = document.getElementById("information");
 
-        const para = document.createElement("p");
+        const header = document.createElement("h1");
+        const headerText = document.createTextNode(arr[0])
+        header.append(headerText);
 
-        const text = document.createTextNode(arr);
+        const height = document.createElement("p");        
+        const heightText = document.createTextNode(`Height: ${arr[1]}`)
+        height.appendChild(heightText);
 
-        para.append(text);
+        const gender = document.createElement("p");
+        const genderText = document.createTextNode(`Gender: ${arr[2]}`)
+        gender.append(genderText);
 
-        info.append(para);
+        const age = document.createElement("p");
+        const ageText = document.createTextNode(`Birth Year: ${arr[3]}`)
+        age.append(ageText);
 
+        const planet = document.createElement("p");
+        const planetText = document.createTextNode(`Home Planet: ${arr[4]}`)
+        planet.append(planetText);
 
+        info.appendChild(header);
+        info.append(height);
+        info.append(gender);
+        info.append(age);
+        info.append(planet);
     }
 }
