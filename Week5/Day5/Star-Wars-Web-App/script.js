ce2a783cd84076f1f9e67d4553fefbfd46d90471
ziv talmon <zivtalmon89@gmail.com>
2022-11-11 23:14:02 +0200

    data appended to page but fetch request time is very long, nedd to find solution, loading animation still required

--- Week5/Day5/Star-Wars-Web-App/script.js
+++ Week5/Day5/Star-Wars-Web-App/script.js
@@ -2,14 +2,15 @@ const button = document.getElementById("search");
 
 button.addEventListener("click", findPerson);
 
-async function findPerson (e)  {
-    e.preventDefault();
+const info = document.getElementById("information");
+
+async function findPerson (e)  {    
     const number = Math.floor(Math.random() * 60) + 1;
 
     const name = await fetch(`https://www.swapi.tech/api/people/${number}`)
     const response1 = await name.json();
     const result1 = await response1.result.properties.name;
-    
+        
     const height = await fetch(`https://www.swapi.tech/api/people/${number}`)
     const response2 = await height.json();
     const result2 = await response2.result.properties.height;
@@ -22,49 +23,72 @@ async function findPerson (e)  {
     const response4 = await age.json();
     const result4 = await response4.result.properties.birth_year;
 
-    const planet = await fetch(`https://www.swapi.tech/api/planets/${number}`)
+    const planet = await fetch(`https://www.swapi.tech/api/people/${number}`)
     const response5 = await planet.json();
-    const result5 = await response5.result.properties.name;
+    const getPlanet = await response5.result.properties.homeworld;
+    const result = await fetch(getPlanet);
+    const resultObj = await result.json();
+    const result5 = await resultObj.result.properties.name;
 
     async function data () {
+        const promiseArr = [result1, result2, result3, result4, result5];
         try {
-            const allPromises = Promise.all([result1, result2, result3, result4, result5])
+            const allPromises = Promise.all(promiseArr)
             const promises = await allPromises;
-            const result = await promises;
-            appendToPage(result);
-        }catch {
+            appendToPage(promises);
+        }catch (Error) {
+            let error = document.createElement("h1");
 
+            let errorText = document.createTextNode("Oh No! That person isnt available!")
+
+            error.style.textAlign = "center";
+
+            error.append(errorText);
+
+            info.append(error);
         }
     }
     return data();
 
     function appendToPage (arr) {
-        const info = document.getElementById("information");
-
         const header = document.createElement("h1");
         const headerText = document.createTextNode(arr[0])
         header.append(headerText);
 
-        const height = document.createElement("p");        
+        const height = document.createElement("h2");        
         const heightText = document.createTextNode(`Height: ${arr[1]}`)
         height.appendChild(heightText);
 
-        const gender = document.createElement("p");
+        const gender = document.createElement("h2");
         const genderText = document.createTextNode(`Gender: ${arr[2]}`)
         gender.append(genderText);
 
-        const age = document.createElement("p");
+        const age = document.createElement("h2");
         const ageText = document.createTextNode(`Birth Year: ${arr[3]}`)
         age.append(ageText);
 
-        const planet = document.createElement("p");
+        const planet = document.createElement("h2");
         const planetText = document.createTextNode(`Home Planet: ${arr[4]}`)
         planet.append(planetText);
 
-        info.appendChild(header);
+
+        info.append(header);
         info.append(height);
         info.append(gender);
         info.append(age);
         info.append(planet);
-    }
+        
+        button.addEventListener("click", clearPage);
+        
+        function clearPage () {
+            info.removeChild(header);
+            info.removeChild(height);
+            info.removeChild(gender);
+            info.removeChild(age);
+            info.removeChild(planet);
+        }
+    }  
 }
+
+
+
