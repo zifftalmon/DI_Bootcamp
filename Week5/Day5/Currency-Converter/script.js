790e712f305aa398ae660cb61d12f12af3fcba12
ziv talmon <zivtalmon89@gmail.com>
2022-11-12 18:08:16 +0200

    function to calculate currencies added, functions added to append to result div and clear result div

--- Week5/Day5/Currency-Converter/script.js
+++ Week5/Day5/Currency-Converter/script.js
@@ -1,41 +1,54 @@
-// api key https://v6.exchangerate-api.com/v6/20a5025d282a16c6561cb0a3/latest/USD
+const selects = document.querySelectorAll("select");
 
-const form = document.getElementById("form");
-
-// form.addEventListener("submit", getCurrnecy);
-
-let ratesKeysArray = [];
-
-const firstSelect = document.getElementById("firstSelect")
-const secondSelect = document.getElementById("secondSelect")
-
-
-async function getRates () {
+async function getRatesKey () {
     const currencyPromise = await fetch("https://v6.exchangerate-api.com/v6/20a5025d282a16c6561cb0a3/latest/USD");
     const currencyObj = await currencyPromise.json();
     const currency = await currencyObj.conversion_rates;
-    const keysArr = Object.keys(currency);
-    keysArr.map(element => console.log(element));
-    appendOptionsFrom(keysArr);
-    appendOptionsTo(keysArr);
-}
-getRates();
-
-function appendOptionsFrom (arr) {
-    arr.map((element) => {
+    const keysArr = Object.entries(currency);
+    keysArr.map((element) => {
         let option = document.createElement("option");
-        let optionText = document.createTextNode(element);
+        let optionText = document.createTextNode(element[0]);
         option.append(optionText);
-        firstSelect.append(option);
-    })
-}
-
-
-function appendOptionsTo (arr) {
-    arr.map((element) => {
+        selects[0].append(option);
+    });
+    keysArr.map((element) => {
         let option = document.createElement("option");
-        let optionText = document.createTextNode(element);
+        let optionText = document.createTextNode(element[0]);
         option.append(optionText);
-        secondSelect.append(option);
-    })
+        selects[1].append(option);
+    });
+}
+getRatesKey();
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", getCurrnecy);
+
+const number = document.getElementById("number");
+
+const resultDiv = document.getElementById("result");
+
+function getCurrnecy(e) {
+    e.preventDefault();
+    console.log(number.value);
+    resultCurrnecy();
+    
+    async function resultCurrnecy () {
+        const request = await fetch(`https://v6.exchangerate-api.com/v6/20a5025d282a16c6561cb0a3/pair/${selects[0].value}/${selects[1].value}`);
+        const requestJson = await request.json();
+        const conversionRate = await requestJson.conversion_rate;
+        let result = (conversionRate * number.value);
+        
+        const resultTitle = document.createElement("h3");
+        const resultText = document.createTextNode(result);
+        resultTitle.appendChild(resultText);
+        resultDiv.appendChild(resultTitle);
+
+        form.addEventListener("click", clearDiv);
+        
+        function clearDiv () {
+            resultDiv.removeChild(resultTitle);
+        }
+    }
+        
 }
