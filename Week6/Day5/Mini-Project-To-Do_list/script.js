d726633f826d5abc6f8918ffa103c41a106b29aa
ziv talmon <zivtalmon89@gmail.com>
2022-11-20 01:46:33 +0200

    foreach for adding the datat on the page removed and data was changed to an object format, to be uploaded to the localstorage, if atatement for preventing blank inputs

--- Week6/Day5/Mini-Project-To-Do_list/script.js
+++ Week6/Day5/Mini-Project-To-Do_list/script.js
@@ -4,24 +4,44 @@ form.addEventListener("submit", addTask)
 
 const body = document.body
 
+let todoItems = [];
+
 async function addTask (e) {
     e.preventDefault();
-    
-    let name = document.forms[0].elements.taskName.value
-    
-    let desc = document.forms[0].elements.taskDesc.value
- 
-    let start = document.forms[0].elements.startDate.value
 
+    let name = document.forms[0].elements.taskName.value
+    let desc = document.forms[0].elements.taskDesc.value
+    let start = document.forms[0].elements.startDate.value
     let end = document.forms[0].elements.endDate.value
 
-    let taskInfo = [`Name : ${name}`, `Description : ${desc}`, `Start date and time : ${start}`, `End date and time : ${end}` ]
+    if (name,desc,start,end != "") {    
+        let taskInfo = {
+         Name: `${name}`,
+         Description: `${desc}`,
+         Startdateandtime : `${start}`,
+         Enddateandtime : `${end}`,
+         Done: false
+        }
+         console.log(taskInfo);
+         todoItems.push(taskInfo)
+         localStorage.setItem(taskInfo.Name, JSON.stringify(taskInfo))
+         console.log(todoItems);
+    } else {
+        alert("write something!")
+    }
 
-    taskInfo.forEach((element => {
-        let infoDiv = document.createElement("div")
-        let infoPara = document.createElement("p")
-        infoPara.innerText = element
-        infoDiv.append(infoPara)
-        body.append(infoDiv)
-    }))
-}
+    }
+
+
+
+    
+    // let check = document.createElement("input")
+    // let checkLabel = document.createElement("label")
+    // check.type="checkbox"
+    // check.id="taskCheck"
+    // checkLabel.htmlFor = "taskCheck"
+    // checkLabel.appendChild(document.createTextNode('Complete'))
+    // infoDiv.append(check)
+    // infoDiv.append(checkLabel)
+    
+
