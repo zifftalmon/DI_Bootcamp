ca83e3e3d021c3b999a09a601f16a4f3cbd2842a
ziv talmon <zivtalmon89@gmail.com>
2022-11-20 15:08:41 +0200

    exercise xp complete

---
+++ Week7/Day1/ExerciseXp1-W7D1/script.js
@@ -0,0 +1,19 @@
+// function addition (x,y) {
+//     return x+y
+// }
+
+// const a = 5;
+// const b = 10;
+
+// console.log(`hello you are ${addition(a,b)}`);
+
+// console.log('hi how are you');
+
+// const a = 5;
+// const b = 10;
+// let x;
+// const c = 6;
+// x = a + b;
+// console.log(x);
+// console.log(x+c);
+
