ae4edd5076527fa3bd46582744438f127d69fa6d
ziv talmon <zivtalmon89@gmail.com>
2022-11-23 15:03:55 +0200

    exercise complete

---
+++ Week7/Day3/ExerciseXp-W7D3/fs.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+// exercise1
+
+// fs.readFile('./text','utf-8',(err,data) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(data);
+//     }
+// })
+
+//exercise2
+
+// fs.writeFile('./data', 'bla bla', (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+// })
+
+// fs.appendFile('./data', 'more bla bla bla', (err) => {
+//     if(err) {
+//         console.log(err);
+//     }
+// })
+
+// fs.unlink('./data', (err) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log('deletion complete');
+//     }
+// })
