f89cdedf3a0e71ffcf21d1475b394d358f637b6a
ziv talmon <zivtalmon89@gmail.com>
2022-11-24 17:14:42 +0200

    server file

--- Week7/Mini-Project-Shopping-List/server.js
+++ Week7/Mini-Project-Shopping-List/server.js
@@ -1,15 +1,29 @@
-const exp = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 dotenv.config();
-const app = exp();
-app.use(cors());
+const app = express();
+app.set('view engine','ejs');
 
-app.listen(process.env.PORT, () => {
+app.use(cors());
+app.use('/', express.static(__dirname+'/public'));
+
+app.listen(process.env.PORT||8080, () => {
     console.log(`run on port ${process.env.PORT}`);
 })
 
-app.get('/products', (req,res) => {
-    res.send('hello')
+app.get('/form', (req,res) => {
+    res.render('index')
 })
+
+app.post('/form', (req,res) => {
+    res.render('cart')
+})
+
+
+
+
+// app.get('/form', (req,res) => {
+//     res.send('/public')
+// })
