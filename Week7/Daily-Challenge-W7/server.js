7042d68bf7b41c3a64a2779a9171d2b13717740c
ziv talmon <zivtalmon89@gmail.com>
2022-11-27 17:25:37 +0200

    server file

--- Week7/Daily-Challenge-W7/server.js
+++ Week7/Daily-Challenge-W7/server.js
@@ -1,18 +1,91 @@
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
+const dotenv = require('dotenv')
+const bcrypt = require('bcrypt')
+const fs = require('fs')
+
+dotenv.config();
 
 const app = express();
 app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.listen(3000, () => {
-    console.log('run on port 3000');
+app.use('/', express.static(__dirname + '/public'))
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`run on port ${process.env.PORT||8080}`);
 })
 
-app.use(express.static(__dirname + '/public'))
+app.post('/login', (req,res) => {
+    const {userName, password} = req.body
+    let users = []
+    try{
+        const f = fs.readFileSync('./users')
+        users = JSON.parse(f.toString())
+    }
+    catch(err) {
+        console.log(err);
+    }
+    if(!isUserExist(users, userName)) {
+        return res.json({msg:'username is not registered'})
+    }
 
-// app.get('/', (req, res) => {
-//     res.send()
-// })
+    const userData = users.find(user => {
+        return user.username == userName
+    })
+    console.log(userData);
+
+    const match = bcrypt.compareSync(password,userData.password)
+
+    if (!match) {
+        return res.json({msg: 'wrong password'})
+    }
+    res.json({msg:'login succesful'})
+})
+
+app.post('/register', (req,res) => {
+    console.log(req.body);
+    
+    let users = [];
+    
+    try{
+        const f = fs.readFileSync('./users')
+        users = JSON.parse(f.toString())
+    }
+    catch(e) {
+        console.log(e);
+    }
+    
+    if(isPassExist(users, req.body.password) || isUserExist(users, req.body.username)) {
+        return res.json({msg: 'username or password already'})
+    } else {}
+
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(req.body.password, salt)
+    
+    req.body.password = hash
+    users.push(req.body)
+    
+    fs.writeFile('./users', JSON.stringify(users), err => {
+        if(err) {
+            console.log(err);
+        }
+    })
+        res.json({msg:'hello your account is now created!'})
+})
+
+const isPassExist = (arr, password) => {
+    const filterUsers = arr.filter(user => {
+        return user.password == password
+    })
+    return (filterUsers.length > 0) ? true : false
+}
+
+const isUserExist = (arr, userName) => {
+    const filterUsers = arr.filter(user => {
+        return user.username == userName
+    })
+    return (filterUsers.length > 0) ? true : false
+}
