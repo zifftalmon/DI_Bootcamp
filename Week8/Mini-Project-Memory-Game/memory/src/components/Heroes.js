c6286c6c6355811c4d5bb0318e2eab4769bbb5c2
ziv talmon <zivtalmon89@gmail.com>
2022-12-06 18:43:52 +0200

    console.logs removed

--- Week8/Mini-Project-Memory-Game/memory/src/components/Heroes.js
+++ Week8/Mini-Project-Memory-Game/memory/src/components/Heroes.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class Heroes extends Component {
     constructor(props) {
-        // console.log(props);
     super(props)
         this.state = {
             score: 0,
@@ -14,11 +13,9 @@ class Heroes extends Component {
                 return item.image
             })
         }
-        // console.log(this.state.clicked);
     }
 
     handleClick = (e) => {
-        // console.log(e);
         if(this.props.heroes[e.target.id].clicked === false) {
             this.setState({score:this.state.score + 1})
             this.setState({clicked:this.props.heroes[e.target.id].clicked = true})
@@ -27,7 +24,6 @@ class Heroes extends Component {
             this.setState({score:0})
             this.setState({clicked:this.props.heroes.map(item => { return item.clicked = false})})
         }
-        console.log(this.props);
     }
 
     render() {
@@ -52,32 +48,8 @@ class Heroes extends Component {
             }
         </div>
         </>
-    )
-    
+        )
     }
-    //     return (
-    //     <>
-    //     <div className='scoreDiv'>
-    //     <h1>superheroes memory game</h1>
-    //     <h4>Score:{this.state.score}     Top Score:{this.state.topScore}</h4>
-    //     </div>
-    //     <h2>Get points by clicking on an image but don't click on any more than once!</h2>
-    //         <div className='container'>
-    //             {this.props.heroes.map((item,i) => {
-    //                 const num = Math.floor(Math.random() * i)
-    //                 console.log(num);
-    //                 return(
-    //                     <div id={item.id} key={i} className='heroDiv'>
-    //                     <img onClick={this.handleClick} alt={this.props.heroes[i].name} src={this.props.heroes[i].image}/>
-    //                     <h5>Name:{this.props.heroes[i].name}</h5>
-    //                     <h5>Occupation:{this.props.heroes[i].occupation}</h5>
-    //                     </div>
-    //                 )
-    //             })}
-    //         </div>
-    //     </>
-    //     );
-    // }
 }
 
 
