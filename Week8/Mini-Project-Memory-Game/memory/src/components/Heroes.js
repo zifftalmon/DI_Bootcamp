869c8924538f3c147e9a90418e5b0d04205534d7
ziv talmon <zivtalmon89@gmail.com>
2022-12-06 18:42:34 +0200

    challenge complete apart from making the card to shuffle, i just couldn't crack it

--- Week8/Mini-Project-Memory-Game/memory/src/components/Heroes.js
+++ Week8/Mini-Project-Memory-Game/memory/src/components/Heroes.js
@@ -2,43 +2,83 @@ import React, { Component } from 'react';
 
 class Heroes extends Component {
     constructor(props) {
-        console.log(props);
-        super(props)
+        // console.log(props);
+    super(props)
         this.state = {
             score: 0,
             topScore: 0,
+            clicked: this.props.heroes.map(item => {
+                return item.clicked
+            }),
+            image: this.props.heroes.map(item => {
+                return item.image
+            })
         }
-        this.handleClick = this.handleClick.bind(this)
+        // console.log(this.state.clicked);
     }
-    
-    handleClick(e) {
-        this.setState({score: this.state.score + 1})
+
+    handleClick = (e) => {
+        // console.log(e);
+        if(this.props.heroes[e.target.id].clicked === false) {
+            this.setState({score:this.state.score + 1})
+            this.setState({clicked:this.props.heroes[e.target.id].clicked = true})
+        }else{
+            this.setState({topScore:this.state.score})
+            this.setState({score:0})
+            this.setState({clicked:this.props.heroes.map(item => { return item.clicked = false})})
+        }
+        console.log(this.props);
     }
 
     render() {
-        return (
+    return(
         <>
         <div className='scoreDiv'>
-        <h1>superheroes memory game</h1>
-        <h4>Score:{this.state.score}     Top Score:{this.state.topScore}</h4>
+            <h1>superheroes memory game</h1>
+            <h4>Score:{this.state.score} Top Score:{this.state.topScore}</h4>
         </div>
         <h2>Get points by clicking on an image but don't click on any more than once!</h2>
-            <div className='container'>
-                {this.props.heroes.map((item,i) => {
-                    const num = Math.floor(Math.random() * i)
-                    console.log(num);
+        <div className='container'>
+            {
+                this.props.heroes.map((item,i) => {
                     return(
-                        <div id={item.id} key={i} className='heroDiv'>
-                        <img onClick={this.handleClick} alt={this.props.heroes[i].name} src={this.props.heroes[i].image}/>
-                        <h5>Name:{this.props.heroes[i].name}</h5>
-                        <h5>Occupation:{this.props.heroes[i].occupation}</h5>
+                        <div key={item.id} className='heroDiv'>
+                            <img value={this.state.clicked[i]} id={i} onClick={this.handleClick} alt={''} src={this.state.image[i]}/>
+                            <h5>Name:{item.name}</h5>
+                            <h5>occupation:{item.occupation}</h5>
                         </div>
                     )
-                })}
-            </div>
+                })
+            }
+        </div>
         </>
-        );
+    )
+    
     }
+    //     return (
+    //     <>
+    //     <div className='scoreDiv'>
+    //     <h1>superheroes memory game</h1>
+    //     <h4>Score:{this.state.score}     Top Score:{this.state.topScore}</h4>
+    //     </div>
+    //     <h2>Get points by clicking on an image but don't click on any more than once!</h2>
+    //         <div className='container'>
+    //             {this.props.heroes.map((item,i) => {
+    //                 const num = Math.floor(Math.random() * i)
+    //                 console.log(num);
+    //                 return(
+    //                     <div id={item.id} key={i} className='heroDiv'>
+    //                     <img onClick={this.handleClick} alt={this.props.heroes[i].name} src={this.props.heroes[i].image}/>
+    //                     <h5>Name:{this.props.heroes[i].name}</h5>
+    //                     <h5>Occupation:{this.props.heroes[i].occupation}</h5>
+    //                     </div>
+    //                 )
+    //             })}
+    //         </div>
+    //     </>
+    //     );
+    // }
 }
+
 
 export default Heroes;
