2378b8ded0cfd22269b19d5d7b965c8e74fdec75
ziv talmon <zivtalmon89@gmail.com>
2023-01-18 12:35:43 +0200

    exercise refactored

--- Week3/Day1/ExerciseXpGold/script.js
+++ Week3/Day1/ExerciseXpGold/script.js
@@ -1,6 +1,5 @@
 let td = document.getElementsByClassName('redTd')
-td[0].style.backgroundColor = 'red'
-td[1].style.backgroundColor = 'red'
-td[2].style.backgroundColor = 'red'
-td[3].style.backgroundColor = 'red'
-td[4].style.backgroundColor = 'red'
+let tdArr = Array.from(td)
+tdArr.map(item => {
+    item.style.backgroundColor = 'red'
+})
