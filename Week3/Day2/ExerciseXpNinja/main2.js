d86cf1edc6a260232cd6599af535229912e9b5d7
ziv talmon <zivtalmon89@gmail.com>
2023-01-20 23:39:16 +0200

    exercise 1 complete, first part of exercise 2 complete

---
+++ Week3/Day2/ExerciseXpNinja/main2.js
@@ -0,0 +1,22 @@
+const button = document.getElementById('emailForm')[1]
+
+const validateEmail = (e) => {
+    const email = e.target.previousElementSibling.value
+
+    const invalid = 'email invalid'
+
+    const symbol1 = email.indexOf('@')
+    
+    if(symbol1 < 1) return alert(`${invalid}, missing @`)
+
+    const symbol2 = email.indexOf('.')
+
+    if(symbol2 <= symbol1 + 2) return alert(`${invalid}, missing dot`)
+
+    if(symbol2 === email.length - 1) return alert(`${invalid}, dot cannot be at the end`)
+
+    alert('email is valid')
+}
+
+
+button.addEventListener('click',validateEmail)
